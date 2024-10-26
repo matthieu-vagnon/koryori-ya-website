06c0fa7cd6755d14eca0b0ef8cff0b2db8760581
M. Vagnon <matthieu.vagnon@predictivelayer.com>
2024-10-26 22:15:04 +0200

    Creating base carousel

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,5 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./Carousel.css";
 
 export default function Carousel() {
-  return <React.Fragment></React.Fragment>;
+  const [active, setActive] = useState(0);
+  const images = [
+    {
+      title: "Sushiro 1",
+      src: "sushiro-1.jpg",
+    },
+    {
+      title: "Sushiro 2",
+      src: "sushiro-2.jpg",
+    },
+  ];
+
+  useEffect(() => {
+    const autoSlide = setTimeout(() => {
+      setActive((prevState) =>
+        prevState < images.length - 1 ? prevState + 1 : 0
+      );
+    }, 5000);
+
+    return () => {
+      clearTimeout(autoSlide);
+    };
+  }, [active]);
+
+  return (
+    <React.Fragment>
+      <div className="carousel-wrapper">
+        <img
+          className="carousel-img"
+          src={images[active].src}
+          alt={images[active].title}
+        />
+      </div>
+    </React.Fragment>
+  );
 }
