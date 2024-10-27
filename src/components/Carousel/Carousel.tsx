79dcf2f8c3e767f3ba3ed4454a3d3130739d07a5
M. Vagnon <matthieu.vagnon@predictivelayer.com>
2024-10-27 19:47:23 +0100

    Adding carousel configuration region

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,10 +1,18 @@
 import React, { useEffect, useState } from "react";
 import "./Carousel.css";
 
+// #region Component configuration
+
 // Sliding speed in ms
 const slideSpeed = 5000;
+
 // Images paths (minimum 2 images)
 const images = ["japan.png", "paris.png", "new-york.png"];
+
+// Noise opacity
+const noiseOpacity = 0.6;
+
+// #endregion
 
 export default function Carousel() {
   const [current, setCurrent] = useState(0);
@@ -38,16 +46,20 @@ export default function Carousel() {
   return (
     <React.Fragment>
       <div className="carousel-container">
-        <div className="noise">
+        <div
+          className="noise"
+          style={{
+            opacity: noiseOpacity,
+          }}
+        >
           <svg height="100%" width="100%" xmlns="http://www.w3.org/2000/svg">
             <filter id="noiseFilter">
               <feTurbulence
-                baseFrequency="5"
+                baseFrequency="1"
                 numOctaves="1"
                 stitchTiles="stitch"
               />
             </filter>
-
             <rect width="100%" height="100%" filter="url(#noiseFilter)" />
           </svg>
         </div>
