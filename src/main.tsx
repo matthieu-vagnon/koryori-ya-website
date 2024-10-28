49ad759c9d6fc161c86156eb26be6b455328a150
M. Vagnon <matthieu.vagnon@predictivelayer.com>
2024-10-28 16:07:33 +0100

    Adding submodule

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import Package from "../package.json";
 import App from "./App.tsx";
+import DemoOverlay from "./components/DemoOverlay/DemoOverlay.tsx";
 import TitleProvider from "./contexts/TitleContext.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <TitleProvider>
+      <DemoOverlay title={Package.title} />
       <App />
     </TitleProvider>
   </React.StrictMode>
