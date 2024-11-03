7fb66fdc6da12282c96eaa92895d9ed03604f0d3
M. Vagnon <matthieu.vagnon@predictivelayer.com>
2024-11-03 12:45:18 +0100

    Removing useless dependency and cleaning up CSS

--- src/components/LandingView/LandingView.tsx
+++ src/components/LandingView/LandingView.tsx
@@ -9,8 +9,10 @@ export default function LandingView() {
         <div className="landing-view-content-container">
           <div className="landing-view-content boxed">
             <img className="landing-view-logo" src="/logo.png" alt="Logo" />
-            <div className="landing-view-title">
-              <h1>アヘャラヌアミ樹差樹擢ほも尾都津れし</h1>
+            <div className="landing-view-title-container">
+              <h1 className="landing-view-title">
+                アヘャラヌアミ樹差樹擢ほも尾都津れし
+              </h1>
               <p className="landing-view-description">
                 アヘャラヌアミ樹差樹擢ほも尾都津れし、やけうより、露等課夜、けてなくつやょ。以尾ほくや氏無らこふのつちまゆヨトフオヒョテま素差はせこしね素名そロンエカルかむせえ「や課雲みほゃ」。ろニヌアナコリ氏瀬他名区、えなろうよな区根模擢。いく譜露。
               </p>
