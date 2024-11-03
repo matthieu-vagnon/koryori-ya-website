d45d289088913bde7d77eb11c3f0f9836a4631bb
M. Vagnon <matthieu.vagnon@predictivelayer.com>
2024-11-03 17:17:08 +0100

    Adding access button on the top right of the landing view

--- src/components/LandingView/LandingView.tsx
+++ src/components/LandingView/LandingView.tsx
@@ -10,17 +10,22 @@ interface LandingViewProps {
 export default function LandingView(props: LandingViewProps) {
   const { mainTitle, subTitle, carouselImages } = props;
 
+  const accessHandler = () => {};
+
   return (
     <React.Fragment>
       <div className="landing-view full-screen-container">
-        <div className="landing-view-content-container">
-          <div className="landing-view-content boxed">
+        <div className="landing-view-content boxed">
+          <div className="landing-view-boxed-container">
             <div className="landing-view-title-container">
               <h1 className="landing-view-text">{mainTitle}</h1>
               <p className="landing-view-text landing-view-description">
                 {subTitle}
               </p>
             </div>
+            <button className="landing-view-access-bt" onClick={accessHandler}>
+              Access
+            </button>
           </div>
         </div>
         <div className="landing-view-carousel">
