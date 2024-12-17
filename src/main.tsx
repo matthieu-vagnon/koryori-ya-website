fdc408e43ab524a393b508e18eb6a430592d6ac0
M. Vagnon <main.mvagnon@icloud.com>
2024-12-17 10:56:18 +0900

    Getting rid of the demo overlay

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,13 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import Package from "../package.json";
-import App from "./App.tsx";
-import DemoOverlay from "./components/DemoOverlay/DemoOverlay.tsx";
-import TitleProvider from "./contexts/TitleContext.tsx";
-import "./index.css";
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import TitleProvider from './contexts/TitleContext.tsx'
+import './index.css'
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <TitleProvider>
-      <DemoOverlay title={Package.title} />
       <App />
     </TitleProvider>
   </React.StrictMode>
-);
+)
