06bf596a86f7f8da7e351d815d59a9661dd96e16
M. Vagnon <main.mvagnon@icloud.com>
2024-12-17 11:21:46 +0900

    Adding images

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,106 @@
-import { Map, Marker, ZoomControl } from "pigeon-maps";
-import React, { useEffect } from "react";
-import Package from "../package.json";
-import "./App.css";
-import japan from "./assets/images/japan.jpg";
-import newYork from "./assets/images/new-york.jpg";
-import paris from "./assets/images/paris.jpg";
-import DualSection from "./components/DualSection/DualSection";
-import Footer from "./components/Footer/Footer";
-import ImageGallery from "./components/ImageGallery/ImageGallery";
-import LandingView from "./components/LandingView/LandingView";
-import LoadAnimation from "./components/LoadAnimation/LoadAnimation";
-import SectionSeparator from "./components/SectionSeparator/SectionSeparator";
-import { useTitle } from "./contexts/TitleContext";
+import { Map, Marker, ZoomControl } from 'pigeon-maps'
+import React, { useEffect } from 'react'
+import Package from '../package.json'
+import './App.css'
+import img9415 from './assets/images/IMG_9415.jpeg'
+import img9514 from './assets/images/IMG_9514.jpeg'
+import img9515 from './assets/images/IMG_9515.jpeg'
+import img9516 from './assets/images/IMG_9516.jpeg'
+import img9517 from './assets/images/IMG_9517.jpeg'
+import img9518 from './assets/images/IMG_9518.jpeg'
+import img9519 from './assets/images/IMG_9519.jpeg'
+import img9520 from './assets/images/IMG_9520.jpeg'
+import img9521 from './assets/images/IMG_9521.jpeg'
+import DualSection from './components/DualSection/DualSection'
+import Footer from './components/Footer/Footer'
+import ImageGallery from './components/ImageGallery/ImageGallery'
+import LandingView from './components/LandingView/LandingView'
+import LoadAnimation from './components/LoadAnimation/LoadAnimation'
+import SectionSeparator from './components/SectionSeparator/SectionSeparator'
+import { useTitle } from './contexts/TitleContext'
 
 function App() {
-  const { setTitle } = useTitle();
+  const { setTitle } = useTitle()
 
   useEffect(() => {
-    setTitle("Home");
-  }, []);
+    setTitle('Home')
+  }, [])
 
   return (
     <React.Fragment>
       <LoadAnimation />
       <LandingView
-        mainTitle="アヘャラヌアミ樹差樹擢ほも尾都津れし"
-        subTitle="アヘャラヌアミ樹差樹擢ほも尾都津れし、やけうより、露等課夜、けてなくつやょ。以尾ほくや氏無らこふのつちまゆヨトフオヒョテま素差はせこしね素名そロンエカルかむせえ「や課雲みほゃ」。ろニヌアナコリ氏瀬他名区、えなろうよな区根模擢。いく譜露。"
-        carouselImages={[japan, paris, newYork]}
+        mainTitle='『Koryori-ya  尾崎』'
+        subTitle='木瓜(ボケ)の花をモチーフにした丸に四方木瓜の家紋がお出迎えします春になると桜の蕾が膨らみ花が咲き白鷺城の周辺は賑やかになりますが…杪冬の頃いち早く春のおとずれを知らせてくれるのが紅の色した愛嬌のある木瓜(ボケ)の花花言葉は『情熱』四季を通して春のおとずれのような暖かいゆるりとした空間でお待ちしております'
+        carouselImages={[img9518, img9519, img9415]}
       />
-      <SectionSeparator start="secondary" end="primary" />
+      <SectionSeparator start='secondary' end='primary' />
       <DualSection
-        title="Concept"
-        description="体と心にやさしい創作料理を。"
-        img={{ src: japan, alt: "Japan Image" }}
-        background="primary"
+        title='Concept'
+        description='体と心にやさしい創作料理を。'
+        img={{ src: img9519, alt: 'Koryoriya Ozaki Image 9520' }}
+        background='primary'
       />
-      <SectionSeparator start="primary" end="secondary" />
+      <SectionSeparator start='primary' end='secondary' />
       <ImageGallery
         images={[
           {
-            src: japan,
-            description: "Japan image",
+            src: img9514,
+            description: ''
           },
           {
-            src: paris,
-            description: "Paris image",
+            src: img9515,
+            description: ''
           },
           {
-            src: newYork,
-            description: "New-York image",
+            src: img9516,
+            description: ''
           },
+          {
+            src: img9517,
+            description: ''
+          },
+          {
+            src: img9518,
+            description: ''
+          },
+          {
+            src: img9519,
+            description: ''
+          },
+          {
+            src: img9520,
+            description: ''
+          },
+          {
+            src: img9521,
+            description: ''
+          },
+          {
+            src: img9415,
+            description: ''
+          },
+          {
+            src: img9415,
+            description: ''
+          }
         ]}
-        background="secondary"
+        background='secondary'
       />
-      <SectionSeparator start="secondary" end="primary" />
+      <SectionSeparator start='secondary' end='primary' />
       <DualSection
-        title="Access"
-        description="世界遺産姫路城がそびえ立つ城下町にひっそりと。"
-        img={{ src: paris, alt: "Paris Image" }}
-        background="primary"
+        title='Access'
+        description='世界遺産姫路城がそびえ立つ城下町にひっそりと。'
+        img={{ src: img9516, alt: 'Koryoriya Ozaki Image 9517' }}
+        background='primary'
         mirror
       >
-        <p className="access-intro">
+        <p className='access-intro'>
           〒670-0017 兵庫県姫路市福中町3番地小寺ビル１階
           <br />
-          Tel.: <a href="tel:070-9055-5210">070-9055-5210</a>
+          Tel.: <a href='tel:070-9055-5210'>070-9055-5210</a>
         </p>
-        <div className="access-map-container">
+        <div className='access-map-container'>
           <Map
             defaultCenter={[34.83258027024389, 134.68785965398067]}
             defaultZoom={18}
@@ -75,14 +109,11 @@ function App() {
             metaWheelZoom
           >
             <ZoomControl />
-            <Marker
-              width={50}
-              anchor={[34.83258027024389, 134.68785965398067]}
-            />
+            <Marker width={50} anchor={[34.83258027024389, 134.68785965398067]} />
           </Map>
         </div>
       </DualSection>
-      <SectionSeparator start="primary" end="tertiary" />
+      <SectionSeparator start='primary' end='tertiary' />
       <Footer
         access={
           <span>
@@ -90,27 +121,27 @@ function App() {
             <br />
             兵庫県姫路市福中町3番地小寺ビル１階
             <br />
-            Tel.: <a href="tel:070-9055-5210">070-9055-5210</a>
+            Tel.: <a href='tel:070-9055-5210'>070-9055-5210</a>
           </span>
         }
         socials={[
           {
-            title: "Instagram",
-            url: "https://www.instagram.com/koryoriya_ozaki",
-          },
+            title: 'Instagram',
+            url: 'https://www.instagram.com/koryoriya_ozaki'
+          }
         ]}
         about={
           <span>
-            Made by{" "}
-            <a href="https://www.mvagnon.dev" target="_blank" rel="noreferrer">
+            Made by{' '}
+            <a href='https://www.mvagnon.dev' target='_blank' rel='noreferrer'>
               {Package.author}
             </a>
           </span>
         }
-        background="tertiary"
+        background='tertiary'
       />
     </React.Fragment>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/DualSection/DualSection.tsx
+++ src/components/DualSection/DualSection.tsx
@@ -1,68 +1,62 @@
-import React, { ReactNode, useState } from "react";
-import { Variant } from "../../shared/types/Variant";
-import "./DualSection.css";
+import React, { ReactNode, useState } from 'react'
+import { Variant } from '../../shared/types/Variant'
+import './DualSection.css'
 
 interface DualSectionProps {
-  title: string;
-  description: string;
-  img: { src: string; alt: string };
-  background: Variant;
-  mirror?: boolean;
-  children?: ReactNode;
+  title: string
+  description: string
+  img: { src: string; alt: string }
+  background: Variant
+  mirror?: boolean
+  children?: ReactNode
 }
 export default function DualSection(props: DualSectionProps) {
-  const { title, description, img, background, mirror, children } = props;
-  const [open, setOpen] = useState(false);
-  const [contentHeight, setContentHeight] = useState(0);
+  const { title, description, img, background, mirror, children } = props
+  const [open, setOpen] = useState(false)
+  const [contentHeight, setContentHeight] = useState(0)
 
   const handleContentVisibilitySwitch = () => {
-    const dsChildrenEl = document.querySelector(".dual-section-children");
+    const dsChildrenEl = document.querySelector('.dual-section-children')
 
     if (dsChildrenEl) {
       setOpen((prevState) => {
         if (prevState === false) {
-          setContentHeight(dsChildrenEl.scrollHeight);
+          setContentHeight(dsChildrenEl.scrollHeight)
         } else {
-          setContentHeight(0);
+          setContentHeight(0)
         }
-        return !prevState;
-      });
+        return !prevState
+      })
     }
-  };
+  }
 
   return (
     <React.Fragment>
-      <div
-        id={title.toLowerCase()}
-        className={`dual-section-children-container ${background}`}
-      >
-        <div className="dual-section-container full-screen-container">
+      <div id={title.toLowerCase()} className={`dual-section-children-container ${background}`}>
+        <div className='dual-section-container full-screen-container'>
           <div
-            className="dual-section boxed"
+            className='dual-section boxed'
             style={{
-              flexDirection: mirror ? "row-reverse" : "row",
+              flexDirection: mirror ? 'row-reverse' : 'row'
             }}
           >
             <img
-              className="dual-section-element dual-section-image"
+              className='dual-section-element dual-section-image'
               src={img.src}
               alt={img.alt}
               style={{
-                borderTopRightRadius: mirror ? 0 : "5px",
-                borderBottomRightRadius: mirror ? 0 : "5px",
-                borderTopLeftRadius: mirror ? "5px" : 0,
-                borderBottomLeftRadius: mirror ? "5px" : 0,
+                borderTopRightRadius: mirror ? 0 : '5px',
+                borderBottomRightRadius: mirror ? 0 : '5px',
+                borderTopLeftRadius: mirror ? '5px' : 0,
+                borderBottomLeftRadius: mirror ? '5px' : 0
               }}
             />
-            <div className="dual-section-element dual-section-content">
-              <h2 className="dual-section-title">{title}</h2>
-              <div className="dual-section-description pp">{description}</div>
+            <div className='dual-section-element dual-section-content'>
+              <h2 className='dual-section-title'>{title}</h2>
+              <div className='dual-section-description pp'>{description}</div>
               {children && (
-                <button
-                  className="button dual-section-button"
-                  onClick={handleContentVisibilitySwitch}
-                >
-                  {open ? "Close" : "Read More"}
+                <button className='button dual-section-button' onClick={handleContentVisibilitySwitch}>
+                  {open ? 'Close' : 'More'}
                 </button>
               )}
             </div>
@@ -70,15 +64,15 @@ export default function DualSection(props: DualSectionProps) {
         </div>
         {children && (
           <div
-            className="dual-section-children-container boxed"
+            className='dual-section-children-container boxed'
             style={{
-              height: contentHeight,
+              height: contentHeight
             }}
           >
-            <div className="dual-section-children">{children}</div>
+            <div className='dual-section-children'>{children}</div>
           </div>
         )}
       </div>
     </React.Fragment>
-  );
+  )
 }
